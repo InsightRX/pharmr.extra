4526ad5d1e937e1a5b245b50ff110ed2a3aa3784
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2026-01-19 09:23:35 -0800

    add tests + TODOs

--- R/get_tool_from_model.R
+++ R/get_tool_from_model.R
@@ -4,6 +4,8 @@
 #' 
 #' @export
 get_tool_from_model <- function(model) {
+  # TODO: Should add some input validation. Currently you can pass pretty much
+  # anything and get "nonmem" in return, e.g., get_tool_from_model(1) works...
   tool <- "nonmem"
   if(inherits(model, "pharmpy.model.external.nlmixr.model.Model")) {
     tool <- "nlmixr"
